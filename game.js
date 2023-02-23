7396d0e420e8eb4f2d10a2911471a5e2d01fca7f
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-02-24 01:31:59 +0200

    thre first working step :')

---
+++ game.js
@@ -0,0 +1,29 @@
+
+window.onload = function(){
+
+
+var start = document.getElementById('start');
+var bounds = document.getElementsByClassName('boundary');
+var end = document.getElementById('end');
+var example = document.getElementsByClassName('example')[0];
+
+
+// console.log(start.innerHTML);
+// console.log(bounds.length);
+// console.log(end.innerHTML);
+// console.log(example);
+
+
+var flag = false;
+var counter = 0;
+
+start.addEventListener('click',function(){
+    for(let i=0;bounds.length -1;i++){
+    bounds[i].style.borderColor = "black";}
+    
+});
+
+
+
+
+}
