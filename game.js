8996d85090eaee6a759a979e577921830ea449bd
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-02-24 02:31:36 +0200

    that too

--- game.js
+++ game.js
@@ -2,8 +2,10 @@
 window.onload = function(){
 
 
+
+
 var start = document.getElementById('start');
-var bounds = document.getElementsByClassName('boundary');
+var bounds =document.getElementsByClassName('boundary');
 var end = document.getElementById('end');
 var example = document.getElementsByClassName('example')[0];
 
@@ -17,11 +19,30 @@ var example = document.getElementsByClassName('example')[0];
 var flag = false;
 var counter = 0;
 
-start.addEventListener('click',function(){
-    for(let i=0;bounds.length -1;i++){
-    bounds[i].style.borderColor = "red";}
-    
+start.addEventListener('mouseover',function(){
+    for (i=0;i <6;i++){
+        bounds[i].style.borderColor = '#ffffff';
+    }
+   
+    var strc=String(counter);
+    example.innerHTML="good for now " + strc;
+
 });
+
+
+end.addEventListener('mouseover',function(){
+    counter += 5;
+    var strc=String(counter);
+    for (i=0;i <6;i++){
+        bounds[i].style.borderColor ='#00ff00';
+    }
+    example.innerHTML="you win!" + strc;
+    
+
+})
+
+
+
 
 
 
